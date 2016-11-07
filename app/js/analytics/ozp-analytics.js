dd6671f1a0e46276f486550c5dfed224d667eddc
Charles Labas <charles.labas@vistronix.com>
2016-11-07 16:50:42 -0500

    ozp-analytics will check for the existence of the window._paq array before pushing events

--- app/js/analytics/ozp-analytics.js
+++ app/js/analytics/ozp-analytics.js
@@ -1,35 +1,41 @@
 'use strict';
 
+var pushEvent = function(event) {
+    if (window._paq) {
+        window._paq.push(event);
+    }
+};
+
 module.exports = {
     trackEvent: function (label, data, agency) {
-        window._paq.push(['trackEvent', label, data, agency]);
+        pushEvent(['trackEvent', label, data, agency]);
     },
 
     trackCategorization: function (label, category, total) {
-        window._paq.push(['trackEvent', label, category, total]);
+        pushEvent(['trackEvent', label, category, total]);
     },
 
     trackSiteSearch: function (label, query, total) {
         query = query.toLowerCase();
-        window._paq.push(['trackSiteSearch', query, label, total]);
+        pushEvent(['trackSiteSearch', query, label, total]);
     },
 
     trackListingCreation: function(listingName, agency){
-        window._paq.push(['trackEvent', 'Listing Creation', listingName, agency]);
+        pushEvent(['trackEvent', 'Listing Creation', listingName, agency]);
     },
 
     trackListingOrgApproval: function(listingName, agency){
-        window._paq.push(['trackEvent', 'Listing Org Approval', listingName, agency]);
+        pushEvent(['trackEvent', 'Listing Org Approval', listingName, agency]);
     },
     trackListingApproval: function(listingName, agency){
-        window._paq.push(['trackEvent', 'Listing Approval', listingName, agency]);
+        pushEvent(['trackEvent', 'Listing Approval', listingName, agency]);
     },
 
     trackListingReview: function(listingName, agency){
-        window._paq.push(['trackEvent', 'Listing Review', listingName, agency]);
+        pushEvent(['trackEvent', 'Listing Review', listingName, agency]);
     },
 
     trackListingReviewView: function(listingName, agency){
-        window._paq.push(['trackEvent', 'Listing Review View', listingName, agency]);
+        pushEvent(['trackEvent', 'Listing Review View', listingName, agency]);
     }
 };
